df50b1780846945c9020c03a24be03df25061226
guoshukun1994 <1775524@163.com>
2023-08-02 23:06:42 +0800

    feat: 添加样式文件demo - @emotion/css和cssModule

--- packages/apps/react-x/src/App.tsx
+++ packages/apps/react-x/src/App.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { css } from '@emotion/css'
+import styles from './app.module.less'
+
+const color: string = 'red'
 
 const App = () => {
-  return (
-    <div className='title'>App, hello</div>
-  )
+	return (
+		<div>
+			<div className="title">App, hello</div>
+			<div
+				className={css`
+					padding: 32px;
+					background-color: pink;
+					&: hover {
+						color: ${color};
+					}
+				`}
+			>
+				hello - emotion
+			</div>
+			<div className={styles.hello}>hello - css module</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
