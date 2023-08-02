87a58692b0b02fa457504762a8b9c565070ca606
guoshukun1994 <1775524@163.com>
2023-08-02 09:07:54 +0800

    chore: 配置webpack - 样式解析loader

--- packages/apps/react-x/scripts/webpack.base.js
+++ packages/apps/react-x/scripts/webpack.base.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // const webpack = require('webpack');
 
 module.exports = (isDev) => ({
@@ -19,7 +19,20 @@ module.exports = (isDev) => ({
                 use: {
                     loader: 'babel-loader',
                 }
-            }
+            },
+            {
+                test: /.(css|less)$/,
+                use: [
+                    // 我们一般情况下, 在开发环境下，我们用 style-loader,方便我们做热更新
+                    // 生产环境下，我们要放在文件里
+                    !isDev ? "style-loader"
+                        : MiniCssExtractPlugin.loader, // MiniCssExtactPlugin 从包含css的js文件中分离打包css文件
+                    // "style-loader",     // 把css-loader生成的css代码通过style标签挂在到页面的header里
+                    "css-loader",      // 将各个css文件合成一个css文件
+                    "postcss-loader", // css界的babel， 比如可以添加css的兼容前缀
+                    "less-loader",    // less转换成css
+                ] // loader加载的顺序是自下而上
+            },
         ]
     },
     resolve: {
@@ -29,7 +42,11 @@ module.exports = (isDev) => ({
         new HTMLWebpackPlugin({
             template: path.resolve(__dirname, "../public/index.html"),
             inject: true,   // 把打包的文件注入到指定html中
+        }),
+        new MiniCssExtractPlugin({
+            // content hash: 根据内容生产的hash 内容变了我才有消除缓存的意义
+            // 还有 chunk hash
+            filename: 'static/css/[name].[contenthash:8].css'
         })
     ]
-
 })
