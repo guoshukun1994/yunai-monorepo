42ebbe0e4cf59c5d8d3af85397b50cfd70822ecf
guoshukun1994 <1775524@163.com>
2023-08-02 23:04:20 +0800

    chore: webpack配置 - css-module\less\css - 三种样式规则兼容处理

--- packages/apps/react-x/scripts/webpack.base.js
+++ packages/apps/react-x/scripts/webpack.base.js
@@ -21,24 +21,56 @@ module.exports = (isDev) => ({
                 }
             },
             {
-                test: /.(css|less)$/,
-                use: [
-                    // 我们一般情况下, 在开发环境下，我们用 style-loader,方便我们做热更新
-                    // 生产环境下，我们要放在文件里
-                    !isDev ? "style-loader"
-                        : MiniCssExtractPlugin.loader, // MiniCssExtactPlugin 从包含css的js文件中分离打包css文件
-                    // "style-loader",     // 把css-loader生成的css代码通过style标签挂在到页面的header里
-                    "css-loader",      // 将各个css文件合成一个css文件
-                    "postcss-loader", // css界的babel， 比如可以添加css的兼容前缀
-                    "less-loader",    // less转换成css
-                ] // loader加载的顺序是自下而上
+                oneOf: [
+                    {
+                        // 定义一下，使用 xxx.module.(less|css)
+                        test: /.module.(less|css)$/,
+                        include: [path.resolve(__dirname, '../src')],
+                        use: [
+                            !isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    importLoaders: 2,
+                                    // 开启 css modules
+                                    modules: {
+                                        localIdentName: '[path][name]__[local]--[hash:base64:4]'
+                                    }
+                                }
+                            },
+                            "postcss-loader",
+                            "less-loader",
+                        ]
+                    },
+                    {
+                        test: /.(css)$/,
+                        use: [
+                            !isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+                            "css-loader",
+                            "postcss-loader",
+                        ]
+                    },
+                    {
+                        test: /.(less)$/,
+                        use: [
+                            // 我们一般情况下, 在开发环境下，我们用 style-loader,方便我们做热更新
+                            // 生产环境下，我们要放在文件里
+                            !isDev ? "style-loader"
+                                : MiniCssExtractPlugin.loader, // MiniCssExtactPlugin 从包含css的js文件中分离打包css文件
+                            // "style-loader",     // 把css-loader生成的css代码通过style标签挂在到页面的header里
+                            "css-loader",      // 将各个css文件合成一个css文件
+                            "postcss-loader", // css界的babel， 比如可以添加css的兼容前缀
+                            "less-loader",    // less转换成css
+                        ] // loader加载的顺序是自下而上
+                    },
+                ]
             },
             {
                 test: /.(png|jpg|jpeg|gif|svg)/,
                 type: 'asset',
                 parser: {
-                    dataUrlCondition:{
-                        maxSize: 10*1024,
+                    dataUrlCondition: {
+                        maxSize: 10 * 1024,
                     }
                 },
                 generator: {
@@ -49,8 +81,8 @@ module.exports = (isDev) => ({
                 test: /.(woff2|eot|ttf|otf)/,
                 type: 'asset',
                 parser: {
-                    dataUrlCondition:{
-                        maxSize: 10*1024,
+                    dataUrlCondition: {
+                        maxSize: 10 * 1024,
                     }
                 },
                 generator: {
@@ -61,8 +93,8 @@ module.exports = (isDev) => ({
                 test: /.(mp4|mp3|webm)/,
                 type: 'asset',
                 parser: {
-                    dataUrlCondition:{
-                        maxSize: 10*1024,
+                    dataUrlCondition: {
+                        maxSize: 10 * 1024,
                     }
                 },
                 generator: {
@@ -83,6 +115,6 @@ module.exports = (isDev) => ({
             // content hash: 根据内容生产的hash 内容变了我才有消除缓存的意义
             // 还有 chunk hash
             filename: 'static/css/[name].[contenthash:8].css'
-        })
+        }),
     ]
 })
