a9c851828ede8cfd51be80b15defa9ff8a824355
guoshukun1994 <1173039846@qq.com>
2023-10-21 18:59:36 +0800

    feat(react-x/render-markdown): markdown - 自动渲染目录结构
    markdown-it 结合 markdown-it-anchor 和 markdown-it-toc-done-right 实现目录自动生成

--- packages/apps/back-end/public/basics/js/handwriting/index.md
+++ packages/apps/back-end/public/basics/js/handwriting/index.md
@@ -1,4 +1,6 @@
-### 1. 手写 Object.create
+### 手写 Object.create
+
+Object.create() 静态方法以一个现有对象作为原型，创建一个新对象。
 
 ```js
 function create(obj) {
@@ -7,3 +9,61 @@ function create(obj) {
     return new F()
 }
 ```
+
+### 手写 instanceof
+
+instanceof 运算符用于判断构造函数的 prototype 属性是否出现在对象的原型链上。实现步骤：
+
+1. 首先获取类型的原型
+2. 然后获取对象的原型
+3. 最后循环判断对象的原型是否等于类型的原型，直到对象原型 `null`，因为原型链最顶层就是 `null` 实现代码：
+
+```js
+function myInstanceof(obj, Fn) {
+    if (!obj) return false
+    let protoForObj = Object.getPrototypeOf(obj)
+    let prototypeForFn = Fn.prototype
+    while (protoForObj) {
+        if (protoForObj === prototypeForFn) return true
+        protoForObj = Object.getPrototypeOf(protoForObj)
+    }
+    return false
+}
+```
+
+### 手写 new
+
+new 就是用来创建构造函数的实例对象的，new 的主要执行操作如下：
+
+1. 在内存中创建一个新对象
+2. 将新对象的原型赋值为构造函数的 prototype 对象
+3. 构造函数内部的 this 赋值为这个新对象（即 this 指向新对象）
+4. 执行构造函数内部的代码（给新对象添加属性）
+5. 如果构造函数返回非null的对象，则返回该对象；否则返回刚创建的新对象。
+
+```js
+function myNew() {
+    let newObj = null // 1
+    let constructor = Array.prototype.shift.call(arguments) // 取出第一个参数 判断其是否是一个函数
+    if (typeof constructor !== 'function') {
+        throw new Error('a is not a constructor')
+    }
+    newObj = Object.create(constructor.prototype) // 2
+    const result = constructor.apply(newObj, arguments) // 3和4
+    return typeof result === 'object' || typeof result === 'function' // 5
+        ? result
+        : newObj
+}
+
+function Fn(a) {
+    this.a = 1
+}
+
+Fn.prototype = {
+    b: 2
+}
+
+myNew(fn, 3)
+```
+
+${toc}
--- packages/apps/react-x/src/components/render-markdown/index.tsx
+++ packages/apps/react-x/src/components/render-markdown/index.tsx
@@ -2,8 +2,10 @@ import React, { useRef, useEffect } from 'react'
 import { message } from 'antd'
 import ClipboardJS from 'clipboard'
 import Markdownit from 'markdown-it'
+import MarkdownitAnchor from 'markdown-it-anchor'
+import MarkdownitToc from 'markdown-it-toc-done-right'
 import hljs from 'highlight.js/lib/core'
-import 'highlight.js/styles/ir-black.css'
+import 'highlight.js/styles/rainbow.css'
 import './index.css'
 
 import bash from 'highlight.js/lib/languages/bash'
@@ -23,54 +25,61 @@ hljs.registerLanguage('xml', xml)
 hljs.registerLanguage('shell', shell)
 
 type Props = {
-	mdText: string
+    mdText: string
 }
 const RenderMarkDown = ({ mdText }: Props) => {
-	const mdRef = useRef<HTMLDivElement>(null)
+    const mdRef = useRef<HTMLDivElement>(null)
 
-	const md = new Markdownit({
-		html: true,
-		linkify: true,
-		breaks: true,
-		xhtmlOut: true,
-		typographer: true,
-		highlight: function (str, lang) {
-			const clipboardHtmlStr = `<button class="copy-btn" data-clipboard-action="copy" data-clipboard-target="#copy">复制</button>`
-			try {
-				return (
-					'<pre class="hljs" style="padding: 24px;position:relative"><code id="copy">' +
-					hljs.highlight(lang, str, true).value +
-					'</code>' +
-					clipboardHtmlStr +
-					'</pre>'
-				)
-			} catch (error) {
-				console.error(error)
-			}
-		}
-	})
+    const md = new Markdownit({
+        html: true,
+        linkify: true,
+        breaks: true,
+        xhtmlOut: true,
+        typographer: true,
+        highlight: function (str, lang) {
+            const clipboardHtmlStr = `<button class="copy-btn" data-clipboard-action="copy" data-clipboard-target="#copy">复制</button>`
+            try {
+                return (
+                    '<pre class="hljs" style="padding: 24px;position:relative"><code id="copy">' +
+                    hljs.highlight(lang, str, true).value +
+                    '</code>' +
+                    clipboardHtmlStr +
+                    '</pre>'
+                )
+            } catch (error) {
+                console.error(error)
+            }
+        },
+    })
+        .use(MarkdownitAnchor, {
+            level: [1, 2, 3],
+            permalink: true,
+            permalinkSymbol: '#',
+            permalinkBefore: true,
+        })
+        .use(MarkdownitToc, { containerClass: 'right-toc-table' })
 
-	const html = md.render(mdText)
+    const html = md.render(mdText)
 
-	useEffect(() => {
-		const clipboard = new ClipboardJS('.copy-btn')
+    useEffect(() => {
+        const clipboard = new ClipboardJS('.copy-btn')
 
-		clipboard.on('success', function (e) {
-			message.success('复制成功')
-			e.clearSelection()
-		})
+        clipboard.on('success', function (e) {
+            message.success('复制成功')
+            e.clearSelection()
+        })
 
-		clipboard.on('error', function () {
-			message.error('复制失败')
-		})
-	}, [])
+        clipboard.on('error', function () {
+            message.error('复制失败')
+        })
+    }, [])
 
-	useEffect(() => {
-		if (mdRef.current) {
-			mdRef.current.innerHTML = html
-		}
-	}, [mdText])
-	return <div className="overflow-hidden" ref={mdRef} />
+    useEffect(() => {
+        if (mdRef.current) {
+            mdRef.current.innerHTML = html
+        }
+    }, [mdText])
+    return <div className="overflow-hidden pr-40" ref={mdRef} />
 }
 
 export default RenderMarkDown
